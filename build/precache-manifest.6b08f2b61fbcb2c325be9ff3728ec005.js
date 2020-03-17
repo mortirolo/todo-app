0998a2cc8eeb365899817d2d841c6d0312f93c30
mortirolo <roland.s@rogers.com>
2020-03-17 13:30:42 -0400

    Add first production build code to repo

---
+++ build/precache-manifest.6b08f2b61fbcb2c325be9ff3728ec005.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7afc902f2ceec6deb165eee11bcb72a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ab16cf9c4e5a3d9e70f",
+    "url": "/static/css/main.36fe29b4.chunk.css"
+  },
+  {
+    "revision": "cba56fee918d9d1c4b8e",
+    "url": "/static/js/2.0f65aadb.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.0f65aadb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ab16cf9c4e5a3d9e70f",
+    "url": "/static/js/main.8ddf2d66.chunk.js"
+  },
+  {
+    "revision": "8764050b6aef1f659b38",
+    "url": "/static/js/runtime-main.8c35323f.js"
+  }
+]);
